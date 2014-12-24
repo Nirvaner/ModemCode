b54ff7fea341b5764d5753c2ab84bd2ba147572f
Anton Zorya <Anton.Zorya@gmail.com>
2014-12-24 21:59:30 +0600

    SKD

--- SKD/skd.js
+++ SKD/skd.js
@@ -229,19 +229,19 @@ function unBlinkLight() {
 function sendToPython(doorState, alarmSet, alarmOn){
    console.log("Sending to python");
 
-// var client = net.connect({port: 10000},
-//     function() { 
-//   console.log('connected to python!');
-//   client.write(""+doorState+""+alermSet+""+alarmOn);
-//   client.destroy(); 
-// });
-
-
-var client = new net.Socket();
-    client.connect(10000, '127.0.0.1', function() {
+var client = net.connect({port: 10000},
+    function() { 
+  console.log('connected to python!');
   client.write(""+doorState+""+alermSet+""+alarmOn);
-    client.destroy(); 
-    });
+  client.destroy(); 
+});
+
+
+// var client = new net.Socket();
+//     client.connect(10000, '127.0.0.1', function() {
+//   client.write(""+doorState+""+alermSet+""+alarmOn);
+//     client.destroy(); 
+//     });
 
     
 }
