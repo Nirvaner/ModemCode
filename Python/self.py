1b6e8c8ed9315e1f4fd6ef16bea4586ba3e9181b
Ivan <SetInNirvana@gmail.com>
2015-01-14 11:23:21 +0600

    ;

--- Python/self.py
+++ Python/self.py
@@ -5,9 +5,9 @@ CurDir = "/devir/ModemCode/Python/"
 
 while True:
 	subprocess.call(["sudo","-u","root","-p","root","python",CurDir + "serviceSub.py"])
-	if os.path.exists(CurDir + "service.update"):
-		print "Update serviceSub.py"
-		subprocess.call(["sudo","-u","root","-p","root","mv",CurDir + "serviceSub.py",CurDir + "baks/serviceSub.py.bak"])
-		subprocess.call(["sudo","-u","root","-p","root","mv",CurDir + "service.update",CurDir + "serviceSub.py"]);
+	if os.path.exists(CurDir + "service"):
+		print "Update service.py"
+		subprocess.call(["sudo","-u","root","-p","root","mv",CurDir + "service.py",CurDir + "baks/serviceSub.py.bak"])
+		subprocess.call(["sudo","-u","root","-p","root","mv",CurDir + "service",CurDir + "service.py"]);
 	else:
 		break
