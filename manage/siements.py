88596e5400d37e33bd3c4536da8fb4b2e3055bfe
Ivan <SetInNirvana@gmail.com>
2015-02-03 15:50:16 +0600

    queue > 1000

--- manage/siements.py
+++ manage/siements.py
@@ -183,7 +183,8 @@ def ReadFromPLC(q,firstArrayFromPLC,secondArrayForCheck,bytesToCheck):
                                 firstArrayFromPLC += bytearray(struct.pack("b",int(date.minute)))
                                 firstArrayFromPLC += bytearray(struct.pack("b",int(date.second)))
                                 firstArrayFromPLC += bytearray(struct.pack("i",int(date.microsecond)))
-                                
+                                if len(q) > 100:
+                                        c = q.get()
                                 q.put(firstArrayFromPLC)
                                 time.sleep(hardRead)
                 except Exception as error:
