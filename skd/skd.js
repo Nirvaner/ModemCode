b99d9c9dd12a135db66bc21658284c92c6af5841
nursultan156 <nursultan156@gmail.com>
2015-02-18 11:59:33 +0600

    asd
    asd

--- skd/skd.js
+++ skd/skd.js
@@ -111,6 +111,7 @@ var gpio11 = gpio.export(17, {
     interval: 200,
     ready: function (val) {
         doorState = val;
+		console.log("I AM INSIDE gpio.export(17,...) and val = "+val);
 		console.log("I AM INSIDE gpio.export(17,...) and gpio11.value = "+gpio11.value);
 		console.log("I AM INSIDE gpio.export(17,...) and gpio11.PATH.VALUE = "+gpio11.PATH.VALUE);
 		console.log("I AM INSIDE gpio.export(17,...) and gpio11.currVal = "+gpio11.currVal);
