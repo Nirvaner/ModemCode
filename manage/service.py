9edf375927eaf236899bedcc36612b62d1576380
Ivan <SetInNirvana@gmail.com>
2015-03-03 15:08:24 +0600

    sleep setsettings

--- manage/service.py
+++ manage/service.py
@@ -99,6 +99,7 @@ while True:
                 elif response[0:8] == "settings":
                         if siementsSub:
                                 siementsSub.terminate()
+                                time.sleep(5)
                         siementsSub = subprocess.Popen(["sudo","-u","root","-p","root","python",CurDir + "siements.py"])
                         i = 0
                         while True:
