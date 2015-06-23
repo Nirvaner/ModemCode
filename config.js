60bfc9dae1036b8383a1e636d7cef254b8cb0170
Nirvaner <SetInNirvana@gmail.com>
2015-06-23 12:15:44 +0600

    Config

--- config.js
+++ config.js
@@ -1,21 +1,10 @@
 var fs = require('fs');
-var modemNumber = 0;
-var version = 0;
-fs.readFile(rootPath + 'set', 'utf8', function (error, data) {
-    if (error){
-        console.log('GetModemNumberError: ' + error);
-    }else{
-        var arr = data.split('\n');
-        modemNumber = arr[0];
-        version = arr[1];
-    }
-});
-module.exports = {
-    ModemNumber: modemNumber,
-    Version: version,
+var config = {
+    ModemNumber: 0,
+    Version: 0,
     Addresses: [
-        '89.218.66.54',
-        'devir.kz',
+        //'89.218.66.54',
+        //'devir.kz',
         'localhost'
     ],
     ServicePort: 10001,
@@ -24,3 +13,16 @@ module.exports = {
     SoundPin: 15,
     LitePin: 16
 };
+module.exports = function(callback) {
+    fs.readFile(rootPath + 'set', 'utf8', function (error, data) {
+        if (error) {
+            console.log('GetModemNumberError: ' + error);
+        } else {
+            var arr = data.split('\n');
+            config.ModemNumber = arr[0];
+            config.Version = arr[1];
+            callback();
+        }
+    });
+    return config;
+}
--- manage.js
+++ manage.js
@@ -3,18 +3,20 @@ global.rootRequire = function (path) {
 };
 global.rootPath = __dirname + '/';
 
+var spawn = require('child_process').spawn;
+function SysRestart() {
+    spawn('sudo', ['-u', 'root', '-p', 'root', 'reboot']);
+}
+
 var net = new require('net');
-var config = rootRequire('config.js');
 var netServer = net.Socket();
+var netSiements = net.Socket();
+var netSkd = net.Socket();
 
 var addressIndex = 0;
-function ConnectToServer(){
-        netServer.connect(config.ServicePort, config.Addresses[addressIndex]);
+function ConnectToServer() {
+    netServer.connect(config.ServicePort, config.Addresses[addressIndex]);
 }
-netServer.on('connect', function () {
-    console.log('Connected: ' + config.Addresses[addressIndex] + ':' + config.ServicePort);
-    Manage();
-});
 netServer.on('error', function () {
     console.log('Server not response: ' + config.Addresses[addressIndex]);
     if (addressIndex < config.Addresses.length - 1) {
@@ -22,24 +24,45 @@ netServer.on('error', function () {
         ConnectToServer();
     }
     else {
+        // Переподключаем модем
         addressIndex = 0;
     }
 });
-netServer.on('close', function (){
-   console.log('ServerSocket is closed..')
+netServer.on('close', function () {
+    console.log('ServerSocket is closed..')
 });
-netServer.on('data', function(data){
-    console.log(data.toString());
+netServer.on('connect', function () {
+    console.log('Connected: ' + config.Addresses[addressIndex] + ':' + config.ServicePort);
+    netServer.write(config.ModemNumber + '|' + config.Version + '||' + '1');
+});
+netServer.on('data', function (data) {
+    var s = data.toString();
+    if (s[0] == '0') {
+        netServer.write('0');
+    } else if (s.substring(0, 3) == 'run') {
+        // Запустить скд и симентс
+        netServer.write('0');
+    } else if (s.substring(0, 3) == 'skd') {
+        netServer.write('0');
+    } else if (s.substring(0, 6) == 'reboot') {
+        netServer.write('0');
+        SysRestart();
+    } else if (s.substring(0, 8) == 'datetime') {
+
+    } else if (s.substring(0, 8) == 'settings') {
+
+    }
 });
 
-function Manage(){
-    try{
-        console.log('Manage');
-    }
-    catch(error){
-        console.log('ErrorManage: ');
-        console.log(error);
-    }
+function SendToSiements(data) {
+
+}
+function SendToSKD(data) {
+
 }
 
-ConnectToServer();
+function ModemRestart() {
+
+}
+
+var config = rootRequire('config.js')(ConnectToServer);
