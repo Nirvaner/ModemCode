acc04cb11fd0b291360840ba6fdb4f6eaecaeca1
Nirvaner <SetInNirvana@gmail.com>
2015-06-26 17:04:29 +0600

    run

--- devir.js
+++
@@ -1,5 +0,0 @@
-var spawn = require('child_process').spawn;
-var manage = spawn('node', [__dirname + 'manage.js'], {stdio: 'inherit'});
-manage.on('exit', function(code){
-    console.log('Exit code: ' + code.toString());
-});
