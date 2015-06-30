3d1ae48dc4907467023f6f19fc3cf98db3a7e804
Nirvaner <SetInNirvana@gmail.com>
2015-06-30 12:29:18 +0600

    debug

--- autorun.sh
+++ autorun.sh
@@ -5,4 +5,7 @@ sudo -u root -p root service isc-dhcp-server start
 # Start Access Point 'skd'
 sudo -u root -p root hostapd -B /etc/hostapd/hostapd.conf
 
+# Run sander
+sudo -u root -p root bash /devir/ModemCode/start.sh
+
 exit 0
